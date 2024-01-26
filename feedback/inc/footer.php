b528a40712e2703158acd4ef1c898c79ae5d7181
Matt Hammond <mjhammond94@gmail.com>
2024-01-26 09:53:53 +0000

    Add header and footer content

---
+++ feedback/inc/footer.php
@@ -0,0 +1,11 @@
+  </div>
+</main>
+
+<footer class="text-center mt-5">
+  Copyright &copy; 1994
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ feedback/inc/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <title>Leave Feedback</title>
+</head>
+<body>
+  <nav class="navbar navbar-expand-sm navbar-light bg-light mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="#">Wu-Feed</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="navbarNav" aria-controls="navbarNav"
+      aria-expanded="false" aria-label="toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="/PHP/feedback/index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/PHP/feedback/feedback.php">Feedback</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/PHP/feedback/about.php">About</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <main>
+    <div class="container d-flex flex-column align-items-center">
+
+  
+</body>
+</html>
