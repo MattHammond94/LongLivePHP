bbc0173f03d24dcb5a10d8eedf14d351b9f627e0
Matt Hammond <mjhammond94@gmail.com>
2024-01-26 10:51:45 +0000

    Add Update feedback page

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -1,27 +1,26 @@
 <?php include 'inc/header.php'; ?>
+
+<?php 
+  $sql = 'SELECT * FROM feedback';
+  $result = mysqli_query($conn, $sql);
+  $feedback = mysqli_fetch_all($result, MYSQLI_ASSOC);
+?>
+
   <h2>Feedback</h2>
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corrupti laborum explicabo nemo, quae, ipsam fugit autem quam tempore sunt obcaecati velit debitis nulla reprehenderit possimus aspernatur 
-      perferendis animi odit quas quis commodi nihil vitae aliquid. Accusamus laudantium consequatur ab eveniet dicta maxime similique non eos delectus nemo tenetur ullam nulla quo ipsum quos a nam voluptatem, 
-      fugit quibusdam aliquid voluptates. Maiores ullam recusandae voluptatibus assumenda sint aspernatur aperiam aliquid eos consequuntur quam quidem reprehenderit, optio voluptate ab voluptas atque esse doloribus 
-      explicabo quae vel perspiciatis est harum ipsa? Fugiat cum facere eaque distinctio, aspernatur molestias error dolores possimus ipsum!
+
+  <?php if(empty($feedback)): ?>
+    <p class="lead mt3">There is no feedback</p>
+  <?php endif; ?>
+
+  <?php foreach($feedback as $item): ?>
+  <div class="card my-3 w-75">
+    <div class="card-body text-center">
+      <?php echo $item['body']; ?>
+      <div class="text-secondary mt-2">
+        By <?php echo $item['name']; ?> on <?php echo $item['date']; ?>
+      </div>
     </div>
   </div>
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corrupti laborum explicabo nemo, quae, ipsam fugit autem quam tempore sunt obcaecati velit debitis nulla reprehenderit possimus aspernatur 
-      perferendis animi odit quas quis commodi nihil vitae aliquid. Accusamus laudantium consequatur ab eveniet dicta maxime similique non eos delectus nemo tenetur ullam nulla quo ipsum quos a nam voluptatem, 
-      fugit quibusdam aliquid voluptates. Maiores ullam recusandae voluptatibus assumenda sint aspernatur aperiam aliquid eos consequuntur quam quidem reprehenderit, optio voluptate ab voluptas atque esse doloribus 
-      explicabo quae vel perspiciatis est harum ipsa? Fugiat cum facere eaque distinctio, aspernatur molestias error dolores possimus ipsum!
-    </div>
-  </div>
-  <div class="card my-3">
-    <div class="card-body">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur corrupti laborum explicabo nemo, quae, ipsam fugit autem quam tempore sunt obcaecati velit debitis nulla reprehenderit possimus aspernatur 
-      perferendis animi odit quas quis commodi nihil vitae aliquid. Accusamus laudantium consequatur ab eveniet dicta maxime similique non eos delectus nemo tenetur ullam nulla quo ipsum quos a nam voluptatem, 
-      fugit quibusdam aliquid voluptates. Maiores ullam recusandae voluptatibus assumenda sint aspernatur aperiam aliquid eos consequuntur quam quidem reprehenderit, optio voluptate ab voluptas atque esse doloribus 
-      explicabo quae vel perspiciatis est harum ipsa? Fugiat cum facere eaque distinctio, aspernatur molestias error dolores possimus ipsum!
-    </div>
-  </div>
+  <?php endforeach; ?>
+  
 <?php include 'inc/footer.php'; ?>
